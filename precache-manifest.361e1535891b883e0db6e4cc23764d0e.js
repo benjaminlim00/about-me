9a425a445a5e158c940f90ed7585d929ab81a81f
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-01-19 21:05:51 +0800

    Updates

---
+++ precache-manifest.361e1535891b883e0db6e4cc23764d0e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7842d476ced2a0f874bdb104016f2965",
+    "url": "/about-me/index.html"
+  },
+  {
+    "revision": "65a395445a124754b47f",
+    "url": "/about-me/static/css/main.38e61ef3.chunk.css"
+  },
+  {
+    "revision": "8e6e3a279e41a1c2d817",
+    "url": "/about-me/static/js/2.3b2d63ef.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/about-me/static/js/2.3b2d63ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65a395445a124754b47f",
+    "url": "/about-me/static/js/main.1c81888d.chunk.js"
+  },
+  {
+    "revision": "e2f8a07815204d57a151",
+    "url": "/about-me/static/js/runtime-main.94733e7b.js"
+  },
+  {
+    "revision": "cdce558aec3170a146f8d201b6a0809a",
+    "url": "/about-me/static/media/dev-ben-01.cdce558a.jpg"
+  },
+  {
+    "revision": "bbe659aaa58aad51f7c7347bf1975127",
+    "url": "/about-me/static/media/dev-ben-02.bbe659aa.jpg"
+  },
+  {
+    "revision": "fad9677451a7a508ef336d8dedeec868",
+    "url": "/about-me/static/media/devben.fad96774.jpg"
+  },
+  {
+    "revision": "6f8d345507e607bc80d92919bac09487",
+    "url": "/about-me/static/media/portfolio-01.6f8d3455.jpg"
+  },
+  {
+    "revision": "238af781fa615dcb77e87d8044ce9716",
+    "url": "/about-me/static/media/portfolio-02.238af781.jpg"
+  },
+  {
+    "revision": "216d2ddeee981170884b6950685444fa",
+    "url": "/about-me/static/media/portfolio-03.216d2dde.jpg"
+  },
+  {
+    "revision": "a58fb775c252e4253d32aab13337be33",
+    "url": "/about-me/static/media/portfolio-04.a58fb775.jpg"
+  },
+  {
+    "revision": "84961aff027ddb358e721a472ae4e197",
+    "url": "/about-me/static/media/portfolio-05.84961aff.jpg"
+  },
+  {
+    "revision": "4eadc66ee1128c8644dbfc9e1a00d224",
+    "url": "/about-me/static/media/portfolio-06.4eadc66e.jpg"
+  },
+  {
+    "revision": "7909856bf323a3d7215763bed46d6083",
+    "url": "/about-me/static/media/portfolio-07.7909856b.jpg"
+  },
+  {
+    "revision": "c49509933c876210ee46b9a42fc266e4",
+    "url": "/about-me/static/media/portfolio-08.c4950993.jpg"
+  },
+  {
+    "revision": "b04311b24758eaccc317015265e3e71d",
+    "url": "/about-me/static/media/portfolio-09.b04311b2.jpg"
+  },
+  {
+    "revision": "1c291f59bb1965138c57e9c3f4a48128",
+    "url": "/about-me/static/media/portfolio-10.1c291f59.jpg"
+  },
+  {
+    "revision": "97891b5e64572b2e21e43124857c4979",
+    "url": "/about-me/static/media/services-bg.97891b5e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.20d8958e04d2976f244f8198bb95be73.js"
+  "/about-me/precache-manifest.361e1535891b883e0db6e4cc23764d0e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/about-me/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
